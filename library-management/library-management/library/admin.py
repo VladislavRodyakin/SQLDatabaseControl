ce2f7139d6837fef23baf22b6156c0196823a22d
RodyakinVladislav <rodyakin.vlad@mail.ru>
2024-06-06 18:44:36 +0700

    updated project

---
+++ library-management/library-management/library/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models import Book,StudentExtra,IssuedBook,Author
+# Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Book, BookAdmin)
+
+
+class StudentExtraAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(StudentExtra, StudentExtraAdmin)
+
+
+class IssuedBookAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(IssuedBook, IssuedBookAdmin)
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Author, AuthorAdmin)
